6d7e02c334dd8a1ff7df0643e09ecc8b97ec23cc
Guilherme Tempesta <guilhermettempesta@outlook.com>
2021-08-12 23:42:42 -0300

    Armazenando cache funcionando

--- MaratonandoApp/Client/wwwroot/service-worker.js
+++ MaratonandoApp/Client/wwwroot/service-worker.js
@@ -1,4 +1,44 @@
-// In development, always fetch from the network and do not enable offline support.
-// This is because caching would make development more difficult (changes would not
-// be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+const cacheNamePrefix = 'offline-cache-';
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const cacheNameDynamic = 'dynamic-cache';
+const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
+const offlineAssetsExclude = [/^service-worker\.js$/];
+
+self.importScripts('./service-worker-assets.js');
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(cacheName).then(cache => {
+            const assetsRequests = self.assetsManifest.assets
+                .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+                .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+                .map(asset => new Request(asset.url, { integrity: asset.hash }));
+            assetsRequests.push(new Request('_configuration/MaratonandoApp.Client'));
+
+            cache.addAll(assetsRequests);
+        })
+    );
+});
+
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
+});
+
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(cacheNameDynamic).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        })
+    );
+});
